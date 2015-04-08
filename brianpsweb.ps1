500430b91bc9f0785c3e8174c9596e5b457daff7
byacono <brian.yacono@gmail.com>
2015-04-08 10:14:36 -0700

    Update brianpsweb.ps1

--- brianpsweb.ps1
+++ brianpsweb.ps1
@@ -10,19 +10,19 @@ $Policy = "RemoteSigned"
 If ((get-ExecutionPolicy) -ne $Policy) {
   Write-Host "Script Execution is disabled. Enabling it now"
   Set-ExecutionPolicy $Policy -Force
-  Write-Host "Please Re-Run this script in a new powershell enviroment"
+  Write-Host "Please Re-Run this script in a new powershell environment"
   Exit
 }
 #Installation of IIS Features & Modules
 Add-WindowsFeature -Name Web-Common-Http,Web-Asp-Net,Web-Net-Ext,Web-ISAPI-Ext,Web-ISAPI-Filter,Web-Http-Logging,Web-Request-Monitor,Web-Basic-Auth,Web-Windows-Auth,Web-Filtering,Web-Performance,Web-Mgmt-Console,Web-Mgmt-Compat,RSAT-Web-Server,WAS -IncludeAllSubFeature
 Import-Module WebAdministration
-Enable-WindowsOptionalFeature ñonline ñfeaturename IIS-WebServerRole
+Enable-WindowsOptionalFeature ‚Äìonline ‚Äìfeaturename IIS-WebServerRole
 $webroot=New-Item c:\Stweb -type Directory
 New-Item c:\Stweb\brianweb -type Directory
-Set-Content c:\Stweb\default.htm ìAutomation for the Peopleî
-Set-Content c:\Stweb\brianweb\default.htm ìWeb Test Pageî
+Set-Content c:\Stweb\default.htm ‚ÄúAutomation for the People‚Äù
+Set-Content c:\Stweb\brianweb\default.htm ‚ÄúWeb Test Page‚Äù
 New-Item iis:\apppools\Stweb
-New-Item iis:\Sites\Stweb -physicalpath $webroot -bindings @{protocol=îhttpî;bindingInformation=î:81:î}
+New-Item iis:\Sites\Stweb -physicalpath $webroot -bindings @{protocol=‚Äùhttp‚Äù;bindingInformation=‚Äù:81:‚Äù}
 Set-ItemProperty iis:\Sites\Stweb -Name applicationpool -Value Stweb
 New-Item iis:\Sites\Stweb\brianweb -physicalpath c:\Stweb\brianweb -type Application
 Set-ItemProperty iis:\Sites\Stweb\brianweb -Name applicationpool -Value Stweb
@@ -32,4 +32,4 @@ Start-Sleep -s 10
 #automated test ie window
 $ie=New-Object -com internetexplorer.application
 $ie.visible = $true
-$ie.Navigate(ìhttp://localhost:81/î)
+$ie.Navigate(‚Äúhttp://localhost:81/‚Äù)
